b6d85a06ad7ad063f814669e7bc8cf79ebfd1892
sujit khandagale <sujitkh94@outlook.com>
2023-02-14 16:06:03 +0530

    file merged

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import "./Navbar.scss"
-import MobileMenu from './small-components/MobileMenu';
-
-
-
-import Link from "next/link";
-import mobileMenu from './small-components/MobileMenu';
-function Navbar({childern}) {
-  return (
-    <div>
-<nav className='navbar'>
- <div className="navbar-collection">
-  <MobileMenu></MobileMenu>
-    <div className="navbar-links">
-       <ul className='navlinks-options'>
-        <li><Link className='nav-link' href="/">Home</Link></li>
-        <li><Link className='nav-link'  href="/benefits">Benefits</Link></li>
-        <li><Link className='nav-link'  href="/about">About Me</Link></li>
-        <li><a className='nav-link'  href="">Contact</a></li>
-        <li><button className='button-style-1' >
-          <a href="tel:+Emergency Call" className='p-0 m-0 text-decoration-none'>Emergency Call</a></button></li>
-       </ul>
-    </div>
- </div>
-</nav>
-
-    </div>
-  )
-=======
 import React from 'react';
 import './Navbar.scss';
 
@@ -76,7 +45,5 @@ function Navbar({ childern }) {
 			</nav>
 		</div>
 	);
->>>>>>> b5cf500b56fdef7384363ca136ea5527cb45fe1b
 }
-
 export default Navbar;
--- src/components/small-components/Offers.js
+++ src/components/small-components/Offers.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-"use client"
-import React from "react";
-import "./Offers.scss";
-=======
 import React from 'react';
 import './Offers.scss';
->>>>>>> b5cf500b56fdef7384363ca136ea5527cb45fe1b
 import Image from 'next/image';
 import ambulance from '../../assets/ambulance.png';
 import Arrow from '../../assets/arrow.png';
@@ -14,38 +8,6 @@ import immunity from '../../assets/immunity.png';
 import medicalStaff from '../../assets/medical-team.png';
 
 function Offers({ children }) {
-<<<<<<< HEAD
-
-  return (
-  <div className="container">
-    <div className="section-heading text-center">
-    <h2>What We Offer </h2>
-    </div>
-      <div className="offers">
-      <div className="offers-tab">
-        <Image src={ambulance} width={100} alt="" />
-<h4>Immediate Assistance</h4>
-      </div>
-      <div className="offers-tab">
-      <Image src={Arrow} width={100} alt="" />
-<h4>Less Expensive</h4>
-      </div>
-      <div className="offers-tab">
-      <Image src={medicalStaff} width={100} alt="" />
-<h4>Experienced Staff</h4>
-      </div>
-      <div className="offers-tab">
-      <Image src={immunity} width={100} alt="" />
-<h4>Ideal For Preventive Care</h4>
-      </div>
-      <div className="offers-tab">
-      <Image src={autoimmuneDdisease} width={100} alt="" />
-<h4>Effective Disease Management</h4>
-      </div>
-    </div>
-    </div>
-  );
-=======
 	return (
 		<div className="container">
 			<div className="section-heading text-center">
@@ -75,7 +37,6 @@ function Offers({ children }) {
 			</div>
 		</div>
 	);
->>>>>>> b5cf500b56fdef7384363ca136ea5527cb45fe1b
 }
 
 export default Offers;
