604663b42569453094cf8005f6a14bec0f1f7337
sujitkhandagale <Sujitkh94@outlook.com>
2023-02-19 19:38:26 +0530

    app title change

--- src/app/head.js
+++ src/app/head.js
@@ -1,7 +1,7 @@
 export default function Head() {
 	return (
 		<>
-			<title>Create Next App</title>
+			<title>World's first doctor consultation app</title>
 			<meta
 				content="width=device-width, initial-scale=1"
 				name="viewport"
